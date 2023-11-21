0bbc8e03534ca3281ea0c34f8c3efc10fa35343b
gverduci <g.verduci@gmail.com>
2023-11-21 22:48:14 +0100

    fix test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import TextField from "@mui/material/TextField";
 import { Controller } from "react-hook-form";
 import { usePersonForm } from "./hooks/usePersonForm";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 function App() {
   const { onSubmitHandler, useFormApi } = usePersonForm();
 
   return (
-    <Box marginLeft="50%" marginTop="50%">
+    <Box marginLeft="5%" marginTop="5%">
+      <Typography variant="h4">Person</Typography>
       <form onSubmit={onSubmitHandler}>
         <Controller
           control={useFormApi.control}
